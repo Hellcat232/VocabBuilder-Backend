42b7ebb65fc90440d38d9f687732257c82c6aed3
Hellcat232 <maxkuich1993@gmail.com>
2024-12-03 00:57:32 +0000

    add ms library

--- src/constants/user.js
+++ src/constants/user.js
@@ -1,8 +1,11 @@
+import ms from 'ms';
+
 export const emailRegExp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 export const passwordRegExp = /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/;
 
-export const ACCESS_TOKET_LIFETIME = 15 * 60 * 1000;
+// export const ACCESS_TOKET_LIFETIME = 15 * 60 * 1000;
+export const ACCESS_TOKET_LIFETIME = ms('15m');
 
 // export const REFRESH_TOKEN_LIFETIME = 24 * 60 * 3600 * 1000;
-export const REFRESH_TOKEN_LIFETIME = 86_400_000;
+export const REFRESH_TOKEN_LIFETIME = ms('1d');
